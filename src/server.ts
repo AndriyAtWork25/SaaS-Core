fcec70354807b9441c345ac353e254ca0c8c37e3
AndriyAtWork <andriytsar3388@gmail.com>
2026-02-07 16:33:37 +0100

    chore: bootstrap ts + express + health endpoint

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// server.ts
+import { app } from './app';
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT}`);
+});
